b5965ffc9c44076a120b56a72c417a29e368fd69
cambech <shashadow57@gmail.com>
2025-10-26 14:57:13 +0100

    Optimize SubMapManagerVoidstone to improve the structure

--- src/main/java/team/chisel/client/render/SubmapManagerVoidstone.java
+++ src/main/java/team/chisel/client/render/SubmapManagerVoidstone.java
@@ -19,90 +19,18 @@ import team.chisel.ctmlib.TextureSubmap;
 
 public class SubmapManagerVoidstone extends SubmapManagerBase {
 
-    // TODO there must be a better more generic way to do this...
-    @SideOnly(Side.CLIENT)
-    private class RenderBlocksVoidstone extends RenderBlocksCTM {
-
-        @Override
-        public void renderFaceXNeg(Block block, double x, double y, double z, IIcon icon) {
-            super.renderFaceXNeg(block, x, y, z, icon);
-            renderMinX += 0.005;
-            setOverrideBlockTexture(getBase(x, y, z, ForgeDirection.WEST.ordinal()));
-            super.renderFaceXNeg(block, x, y, z, null);
-            clearOverrideBlockTexture();
-        }
-
-        @Override
-        public void renderFaceXPos(Block block, double x, double y, double z, IIcon icon) {
-            super.renderFaceXPos(block, x, y, z, icon);
-            setOverrideBlockTexture(getBase(x, y, z, ForgeDirection.EAST.ordinal()));
-            renderMaxX -= 0.005;
-            super.renderFaceXPos(block, x, y, z, null);
-            clearOverrideBlockTexture();
-        }
-
-        @Override
-        public void renderFaceYNeg(Block block, double x, double y, double z, IIcon icon) {
-            super.renderFaceYNeg(block, x, y, z, icon);
-            setOverrideBlockTexture(getBase(x, y, z, ForgeDirection.DOWN.ordinal()));
-            renderMinY += 0.005;
-            super.renderFaceYNeg(block, x, y, z, null);
-            clearOverrideBlockTexture();
-        }
-
-        @Override
-        public void renderFaceYPos(Block block, double x, double y, double z, IIcon icon) {
-            super.renderFaceYPos(block, x, y, z, icon);
-            setOverrideBlockTexture(getBase(x, y, z, ForgeDirection.UP.ordinal()));
-            renderMaxY -= 0.005;
-            super.renderFaceYPos(block, x, y, z, null);
-            clearOverrideBlockTexture();
-        }
-
-        @Override
-        public void renderFaceZNeg(Block block, double x, double y, double z, IIcon icon) {
-            super.renderFaceZNeg(block, x, y, z, icon);
-            setOverrideBlockTexture(getBase(x, y, z, ForgeDirection.NORTH.ordinal()));
-            renderMinZ += 0.005;
-            super.renderFaceZNeg(block, x, y, z, null);
-            clearOverrideBlockTexture();
-        }
-
-        @Override
-        public void renderFaceZPos(Block block, double x, double y, double z, IIcon icon) {
-            super.renderFaceZPos(block, x, y, z, icon);
-            setOverrideBlockTexture(getBase(x, y, z, ForgeDirection.SOUTH.ordinal()));
-            renderMaxZ -= 0.005;
-            super.renderFaceZPos(block, x, y, z, null);
-            clearOverrideBlockTexture();
-        }
-
-        public void reset() {
-            this.submap = null;
-        }
-    }
-
-    @SideOnly(Side.CLIENT)
-    private static ThreadLocal<RenderBlocksVoidstone> renderBlocksThreadLocal;
+    private static ThreadLocal<RenderBlocksCTM> renderBlocksThreadLocal;
 
     private static void initStatics() {
         if (renderBlocksThreadLocal == null) {
-            renderBlocksThreadLocal = new ThreadLocal<>();
+            renderBlocksThreadLocal = ThreadLocal.withInitial(RenderBlocksCTM::new);
         }
     }
 
-    private ISubmapManager overlay;
-    private TextureSubmap base;
+    private TextureSubmap bgmap;
 
-    private IIcon getBase(double x, double y, double z, int side) {
-        return TextureType.getVIcon(
-            TextureType.V4,
-            base,
-            MathHelper.floor_double(x),
-            MathHelper.floor_double(y),
-            MathHelper.floor_double(z),
-            side);
-    }
+    private TextureSubmap submap;
+    private TextureSubmap submapSmall;
 
     private String texture;
     private int meta;
@@ -114,37 +42,94 @@ public class SubmapManagerVoidstone extends SubmapManagerBase {
 
     @Override
     public IIcon getIcon(int side, int meta) {
-        return overlay.getIcon(side, meta);
+        return submapSmall.getBaseIcon();
     }
 
     @Override
     public IIcon getIcon(IBlockAccess world, int x, int y, int z, int side) {
-        return overlay.getIcon(world, x, y, z, side);
+        return getIcon(side, world.getBlockMetadata(x, y, z));
     }
 
     @Override
     public void registerIcons(String modName, Block block, IIconRegister register) {
-        overlay = TextureType.getTypeFor(null, modName, texture)
-            .createManagerFor(CarvingUtils.getDefaultVariationFor(block, meta, 0), texture);
-        overlay.registerIcons(modName, block, register);
-        base = new TextureSubmap(register.registerIcon(modName + ":" + "animations/hadesX32"), 2, 2);
+        String path = modName + ":" + texture;
+        if (texture == "voidstone/animated/bevel" || texture == "voidstone/animated/metalborder")
+        	submap = new TextureSubmap(register.registerIcon(path + "-ctm"), 4, 4);
+        else
+        	submap = new TextureSubmap(register.registerIcon(path), 2, 2);
+        submapSmall = new TextureSubmap(register.registerIcon(path), 2, 2);
+        bgmap = new TextureSubmap(register.registerIcon(modName + ":" + "animations/hadesX32"), 2, 2);
     }
 
     @Override
     @SideOnly(Side.CLIENT)
     public RenderBlocks createRenderContext(RenderBlocks rendererOld, Block block, IBlockAccess world) {
         initStatics();
-        RenderBlocksVoidstone rb = renderBlocksThreadLocal.get();
-        if (rb == null) {
-            rb = new RenderBlocksVoidstone();
-            renderBlocksThreadLocal.set(rb);
-        } else rb.reset();
-        RenderBlocks ctx = overlay.createRenderContext(rendererOld, block, world);
-        rb.setRenderBoundsFromBlock(block);
-        if (ctx instanceof RenderBlocksCTM) {
-            rb.submap = ((RenderBlocksCTM) ctx).submap;
-            rb.submapSmall = ((RenderBlocksCTM) ctx).submapSmall;
+        RenderBlocksCTM rb = renderBlocksThreadLocal.get();
+        try {
+            rb.setRenderBoundsFromBlock(block);
+            if (texture == "voidstone/animated/bevel" || texture == "voidstone/animated/metalborder") {
+            	rb.submap = submap;
+            	rb.submapSmall = submapSmall;
+            }
+            return rb;
+        } finally {
+            renderBlocksThreadLocal.remove();
         }
-        return rb;
+    }
+
+    private boolean hadOverride = false;
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void preRenderSide(RenderBlocks renderer, IBlockAccess world, int x, int y, int z, ForgeDirection side) {
+        RenderBlocksCTM rbctm = (RenderBlocksCTM) renderer;
+        if (!rbctm.hasOverrideBlockTexture()) {
+            hadOverride = true;
+            rbctm.setOverrideBlockTexture(
+                TextureType.getVIcon(
+                    TextureType.V4,
+                    bgmap,
+                    MathHelper.floor_double(x),
+                    MathHelper.floor_double(y),
+                    MathHelper.floor_double(z),
+                    side.ordinal()));
+            if (side == ForgeDirection.NORTH) {
+                rbctm.renderFaceZNeg(world.getBlock(x, y, z), x, y, z, null);
+            } else if (side == ForgeDirection.SOUTH) {
+                rbctm.renderFaceZPos(world.getBlock(x, y, z), x, y, z, null);
+            } else if (side == ForgeDirection.WEST) {
+                rbctm.renderFaceXNeg(world.getBlock(x, y, z), x, y, z, null);
+            } else if (side == ForgeDirection.EAST) {
+                rbctm.renderFaceXPos(world.getBlock(x, y, z), x, y, z, null);
+            } else if (side == ForgeDirection.DOWN) {
+                rbctm.renderFaceYNeg(world.getBlock(x, y, z), x, y, z, null);
+            } else {
+                rbctm.renderFaceYPos(world.getBlock(x, y, z), x, y, z, null);
+            }
+            hadOverride = false;
+        }
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void postRenderSide(RenderBlocks renderer, IBlockAccess world, int x, int y, int z, ForgeDirection side) {
+        RenderBlocksCTM rbctm = (RenderBlocksCTM) renderer;
+        rbctm.clearOverrideBlockTexture();
+        if (texture == "voidstone/animated/bevel" || texture == "voidstone/animated/metalborder") {
+            if (side == ForgeDirection.NORTH) {
+                rbctm.renderMinZ -= 0.001;
+            } else if (side == ForgeDirection.SOUTH) {
+                rbctm.renderMaxZ += 0.001;
+            } else if (side == ForgeDirection.WEST) {
+                rbctm.renderMinX -= 0.001;
+            } else if (side == ForgeDirection.EAST) {
+                rbctm.renderMaxX += 0.001;
+            } else if (side == ForgeDirection.DOWN) {
+                rbctm.renderMinY -= 0.001;
+            } else {
+                rbctm.renderMaxY += 0.001;
+            }
+        }
     }
 }
